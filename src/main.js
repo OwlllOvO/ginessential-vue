adb3978852ea051e1ddb3c024cab4fbe1fd59c05
OwlllOvO <OwlllOvO@Outlook.com>
2024-03-11 17:31:15 +0800

    Login.vue and Register.vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 // scss style
 import './assets/scss/index.scss';
+
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
--- src/views/register/Register.vue
+++ src/views/register/Register.vue
@@ -11,24 +11,32 @@
           <b-form>
             <b-form-group label="Name">
               <b-form-input
-                v-model="user.name"
+                v-model="$v.user.name.$model"
                 type="text"
                 placeholder="Enter Username(Option)"
               ></b-form-input>
             </b-form-group>
             <b-form-group label="Phone Number">
               <b-form-input
-                v-model="user.telephone"
+                v-model="$v.user.telephone.$model"
                 type="text"
                 placeholder="Enter Telephone Number"
+                :state="validateState('telephone')"
               ></b-form-input>
+              <b-form-invalid-feedback :state="validateState('telephone')">
+                Phone number format error.
+              </b-form-invalid-feedback>
             </b-form-group>
             <b-form-group label="Password">
               <b-form-input
-                v-model="user.password"
+                v-model="$v.user.password.$model"
                 type="password"
                 placeholder="Enter Password"
+                :state="validateState('password')"
               ></b-form-input>
+              <b-form-invalid-feedback :state="validateState('password')">
+                Your password must be more than 6 characters long.
+              </b-form-invalid-feedback>
             </b-form-group>
             <b-form-group label="">
               <b-button
@@ -45,6 +53,9 @@
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators';
+import customValidator from '@/helper/validator';
+
 export default {
   data() {
     return {
@@ -53,9 +64,30 @@ export default {
         telephone: '',
         password: '',
       },
+      validation: null,
     };
   },
+  validations: {
+    user: {
+      name: {
+
+      },
+      telephone: {
+        required,
+        telephone: customValidator.telephoneValidator,
+      },
+      password: {
+        required,
+        minLength: minLength(6),
+      },
+    },
+  },
   methods: {
+    validateState(name) {
+      // ES6 destructuring assignment
+      const { $dirty, $error } = this.$v.user[name];
+      return $dirty ? !$error : null;
+    },
     register() {
       console.log('register');
     },
